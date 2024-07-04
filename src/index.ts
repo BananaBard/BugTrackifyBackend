1ebd5dc482cd17b627017b39c6b8c79a109fa77c
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-03 22:59:12 -0300

    Installed CORS and Helmet Added json parser, url encoded, cors and helmet to express app, added middleware for handling errors

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,43 @@
 // src/index.js
-import express, { Express, Request, Response } from "express";
+import express, { Errback, ErrorRequestHandler, Express, NextFunction, Request, Response } from "express";
+import cors from 'cors';
 import dotenv from "dotenv";
+import helmet from "helmet";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Bugtrackify Backend");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+
+// Rutas
+app.get('/api/example', (req, res) => {
+  res.send('GET request to the homepage');
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.post('/api/example', (req, res) => {
+  res.json({ message: 'POST request to the homepage' });
+});
+
+app.put('/api/example/:id', (req, res) => {
+  res.json({ message: `PUT request to update item with id ${req.params.id}` });
+});
+
+app.delete('/api/example/:id', (req, res) => {
+  res.json({ message: `DELETE request to delete item with id ${req.params.id}` });
+});
+
+// Middleware de manejo de errores
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
