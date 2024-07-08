0109ae09d95dfabb2f506e9ff4bb899cae8713a8
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-08 18:57:52 -0300

    Services to get user by email from db

---
+++ src/services/users/getUserByEmail.service.ts
@@ -0,0 +1,14 @@
+import supabase from "../../infra/supabaseClient"
+
+const getUserByEmailService = async(email: string) => {
+    const { data, error } = await supabase.from('users').select().match({ email: email });
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    console.log(data.length)
+    return data.length !== 0;
+}
+
+export default getUserByEmailService
