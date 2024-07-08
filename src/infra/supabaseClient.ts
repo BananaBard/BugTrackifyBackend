d98712896d67e596f39dd55e742797cd2540bfa6
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-08 18:58:00 -0300

    Added admin instance

--- src/infra/supabaseClient.ts
+++ src/infra/supabaseClient.ts
@@ -1,7 +1,16 @@
 import {createClient} from '@supabase/supabase-js'
+import { configDotenv } from 'dotenv'
+import { Database } from '../supabase'
+
+configDotenv()
+
 
 const supabaseUrl = 'https://ljgtwhzobcvghjowvitv.supabase.co'
 const supabaseKey = process.env.PUBLIC_KEY!
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient<Database>(supabaseUrl, supabaseKey)
+const supabaseAdminKey = process.env.SECRET_KEY!
+
+const supabaseAdmin = createClient<Database>(supabaseUrl, supabaseAdminKey)
 
 export default supabase
+export {supabaseAdmin}
