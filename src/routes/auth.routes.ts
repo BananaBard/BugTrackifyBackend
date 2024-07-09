898dc9e8d7ab807b2fa1c2a1382977bff7e84da5
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-09 20:14:32 -0300

    Added login with email and sign out routes The sign out routes dont need a services, the only thing it does is remove the token from the cookies

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,13 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { signUpWithEmailController } from "../controllers/signUp.controller";
+import { loginWithEmail } from "../controllers/loginWithEmail.controller";
 
 const authRoutes = Router()
 
 authRoutes.post('/signup', signUpWithEmailController);
+authRoutes.post('/login', loginWithEmail);
+authRoutes.post('/signout', (_req: Request, res: Response, _next: NextFunction) => {
+    return res.clearCookie('access_token').json({message: 'Sign out successfully.'})
+});
 
 export default authRoutes;
