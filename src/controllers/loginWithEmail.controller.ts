51b12b001a30cdc69087fed94b15f53a48272412
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-09 20:13:44 -0300

    Created controller for login with email This controller checks the response from the service, if we dont have an error it sends the user data and set up the cookie for the token, otherwise, it sends an error

---
+++ src/controllers/loginWithEmail.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import loginWithEmailService from "../services/auth/loginWithEmail.service";
+import getErrorMessage from "../utils/getErrorMessage";
+import { supabaseAdmin } from "../infra/supabaseClient";
+
+const loginWithEmail = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+        const resp = await loginWithEmailService({ email, password });
+
+        if (resp.error?.status === 400) return res.status(400).json({ message: 'No user with this email or wrong password, check your credentials again' });
+        const token = resp.data.session?.access_token;
+        const uid = resp.data.user?.id
+        const user = await supabaseAdmin.from('users').select().eq('id', uid!)
+
+        return res
+            .status(200)
+            .cookie('access_token', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'PRODUCTION',
+                sameSite: 'strict',
+                maxAge: 1000 * 60 * 60,
+            })
+            .json({
+                token: token,
+                user: user?.data
+            })
+
+    } catch (error) {
+        return res.status(401).json({ message: getErrorMessage(error) })
+    }
+}
+
+export { loginWithEmail }
