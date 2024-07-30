fcec6ef8525f6c3fa380baebb2aedd13eb356066
Bruno González <brunogonzalez.dev@gmail.com>
2024-07-29 23:00:44 -0300

    fix: Changed responses Sending the responses in correct format, solving an error in the frontend. Now the response is send as json

--- src/controllers/auth/authStatus.controller.ts
+++ src/controllers/auth/authStatus.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { configDotenv } from 'dotenv';
 import { verifyToken } from '../../utils/verifyToken';
 import { getUserByEmailService } from '../../services/users/getUserByEmail.service';
+import getErrorMessage from '../../utils/getErrorMessage';
 
 configDotenv()
 
@@ -9,16 +10,16 @@ const authStatusController = async (req: Request, res: Response) => {
   const token = req.cookies.access_token;
 
   if (!token) {
-    return res.status(401).send('Not authenticated');
+    return res.status(401).json({message: 'Not authenticated'});
   }
 
   try {
     const decoded = verifyToken(token)
     const userEmail = decoded.user_metadata.email;
     const user = await getUserByEmailService(userEmail);
-    res.status(200).send(user);
+    res.status(200).json({user: user});
   } catch (error) {
-    res.status(401).send('Invalid token');
+    res.status(401).json({message: getErrorMessage(error)});
   }
 };
 
