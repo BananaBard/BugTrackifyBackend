0bb931188de79c41838c126666bbd36cea4125d3
Bruno González <brunogonzalez.dev@gmail.com>
2024-08-06 19:02:23 -0300

    feat: Add create incident service INITIAL STATE

---
+++ src/services/incidents/createIncident.service.ts
@@ -0,0 +1,10 @@
+import supabase from "../../infra/supabaseClient";
+import getErrorMessage from "../../utils/getErrorMessage";
+import {Tables} from '../../supabase';
+
+
+const createIncidentService = async ({  }: Tables<'Incidents'>) => {
+    
+}
+
+export {createIncidentService};
