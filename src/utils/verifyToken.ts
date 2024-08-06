c628aed5afaef5df5f7a53eb8855597f355e657d
Bruno González <brunogonzalez.dev@gmail.com>
2024-08-06 19:01:25 -0300

    feat: Remove token if expired

--- src/utils/verifyToken.ts
+++ src/utils/verifyToken.ts
@@ -15,6 +15,9 @@ const verifyToken = (token: string): MyJwtPayload => {
     if (typeof decoded !== 'object' || !('sub' in decoded)) {
       throw new Error('Invalid token');
     }
+    if ((decoded?.exp! * 1000) < Date.now()) {
+      throw new Error('Expired token');
+    }
     return decoded as MyJwtPayload;
   } catch (error) {
     throw new Error('Token verification failed');
