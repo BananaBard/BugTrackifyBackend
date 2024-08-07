e80f1b12ea9fc77edb1a025698e8b78484beae51
Bruno González <brunogonzalez.dev@gmail.com>
2024-08-07 00:22:14 -0300

    feat: Add validation middleware to post new project route

--- src/routes/projects.routes.ts
+++ src/routes/projects.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createProjectController } from "../controllers/projects/createProject.controller";
 import { getUserProjectsController } from "../controllers/projects/getUserProjects.controller";
+import { validate } from "../middlewares/validate.middleware";
+import { projectSchema } from "../zod_schemas/schemas";
 
 const projectsRoutes = Router()
 
-projectsRoutes.post('/new', createProjectController);
+projectsRoutes.post('/new',validate(projectSchema),createProjectController);
 projectsRoutes.get('/getByUserId/:uid', getUserProjectsController);
 
 export {projectsRoutes};
