cb010c0c2fe0cc16cc7f1eb75fa618eac9a86753
Bruno González <brunogonzalez.dev@gmail.com>
2024-08-27 19:08:32 -0300

    feat: Add delete and update routes

--- src/routes/incidents.routes.ts
+++ src/routes/incidents.routes.ts
@@ -3,11 +3,14 @@ import { validate } from "../middlewares/validate.middleware";
 import { incidentSchema } from "../zod_schemas/schemas";
 import { createIncidentController } from "../controllers/incidents/createIncident.controller";
 import { getProjectIncidentsController } from "../controllers/incidents/getProjectIncidents.controller";
+import { updateIncidentController } from "../controllers/incidents/updateIncident.controller";
+import { deleteIncidentController } from "../controllers/incidents/deleteIncident.controller";
 
 const incidentRoutes = Router();
 
 incidentRoutes.post('/new', validate(incidentSchema), createIncidentController);
 incidentRoutes.get('/byProject/:projectId', getProjectIncidentsController)
-
+incidentRoutes.put('/update', updateIncidentController)
+incidentRoutes.delete('/:incidentId', deleteIncidentController)
 
 export {incidentRoutes}
